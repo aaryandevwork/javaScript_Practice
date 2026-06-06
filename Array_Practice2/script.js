24d68b22059050049534140ea87e72e282e5ae07
aaryandevwork <aaryandevwork@gmail.com>
2026-06-06 16:13:53 +0530

    I Practiced : forEach, map, filer, reduce ,some , every , find and findIndex

--- Array_Practice2/script.js
+++ Array_Practice2/script.js
@@ -38,10 +38,10 @@
 // - Discount is 10%
 
 
-let products = [
-  { name: "Laptop", price: 50000 },
-  { name: "Phone", price: 20000 },
-];
+// let products = [
+//   { name: "Laptop", price: 50000 },
+//   { name: "Phone", price: 20000 },
+// ];
 
 //In this original products also made change : 
 // let discountedPrice = products.map((element) =>{
@@ -61,3 +61,140 @@ let products = [
 // });
 
 // console.log(discountedPrice);
+
+//--------------------Ques 3 :  Filter -----------------------
+// // medium :  Filter all even numbers.
+
+// // let nums = [1,2,3,4,5,6,7,8];
+
+// // let evenNums = nums.filter((Element) => Element%2 == 0)
+
+// // console.log(evenNums)
+
+// // hard :  You are given users.
+// // Return only active users.
+
+// // let users = [
+// //   { name: "Anubhav", active: true },
+// //   { name: "Rahul", active: false },
+// //   { name: "Aman", active: true },
+// // ];
+
+// // let activeUsers = users.filter((Element) => Element.active === true)
+
+// // console.log(activeUsers);
+
+// //--------------------Ques 4 :  Reduce -----------------------
+
+
+// //Medium :  Find total sum of array.
+
+// let nums = [10,20,30,40];
+
+// // let sum = nums.reduce((acc,curr) => acc += curr, 0)
+
+// let sum = nums.reduce((acc,curr) => {
+//     acc += curr
+//     return acc;
+// },0)
+
+// console.log(sum)
+
+// //Hard :  Count frequency of elements.
+
+// let fruits = ["apple", "banana", "apple", "orange", "banana", "apple"];
+
+// let counted = fruits.reduce((acc, curr) => {
+//     if(acc[curr]){
+//         acc[curr]++;
+//     }
+//     else{
+//         acc[curr] = 1;
+//     }
+//     return acc;
+// },{})
+
+// console.log(counted)
+
+// //--------------------Ques 5 :  Find -----------------------
+//Mid :  Find first number greater than 50.
+
+// let nums = [20, 35, 60, 80];
+
+// let finded = nums.find((Element) => Element > 50 )
+
+// console.log(finded)
+
+// // hard : Find a user with username `"admin"`.
+
+// let users = [
+//     { username: "rahul" },
+//     { username: "admin" },
+//     { username: "aman" }
+// ];
+
+// let founded = users.find((element) => element.username === "admin")
+
+// console.log(founded)
+
+// //--------------------Ques 6 :  FindIndex -----------------------
+
+// Mid :  Find index of number `90`.
+
+// let nums = [10, 40, 90, 50];
+
+// let index = nums.findIndex((Element) => Element == 90)
+
+// console.log(index+1)
+
+// // Hard Question : Find index of first failed student.
+
+// let students = [
+//   { name: "A", marks: 90 },
+//   { name: "B", marks: 30 },
+//   { name: "C", marks: 70 },
+// ];
+
+// let failedStudent = students.findIndex((Element) => Element.marks < 40);
+
+// console.log(failedStudent+1)
+
+// //--------------------Ques 7 :  some() -----------------------
+// mid :  Check if any number is negative.
+
+// let nums = [10, 20, -5, 40];
+
+// let neg = nums.some((Element) => Element < 0)
+
+// console.log(neg);
+
+// // Hard : Check if any product is out of stock.
+
+// let products = [
+//   { name: "Laptop", stock: 5 },
+//   { name: "Phone", stock: 0 },
+// ];
+
+// let outOfStock = products.some((Element) => Element.stock <= 0)
+// console.log(outOfStock)
+
+// //--------------------Ques 7 :  every() -----------------------
+// Intermediate :  check if all numbers are positive.
+
+let nums = [10, 20, 30, 40];
+
+let allPositive = nums.every((element) => element>0)
+
+console.log(allPositive);
+
+// Hard : Check if all students passed.
+
+let students = [
+  { name: "A", marks: 80 },
+  { name: "B", marks: 45 },
+  { name: "C", marks: 60 },
+];
+
+let allPassed = students.every((element) => element.marks >= 40)
+
+console.log(allPassed)
