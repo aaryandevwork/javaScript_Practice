f97b43bdc21b4a055ad0fb6230474678da026f79
aaryandevwork <aaryandevwork@gmail.com>
2026-06-15 00:21:54 +0530

    question 3 solved

--- phase4/script.js
+++ phase4/script.js
@@ -1,6 +1,6 @@
-// 'use strict'
+'use strict'
 /*
-## Problem 1: Global vs Function `this`
+################################################### Problem 1: Global vs Function `this`
 
 Create a function `showThis()` and print the value of `this` when:
 
@@ -20,7 +20,7 @@ Create a function `showThis()` and print the value of `this` when:
 // showThis()
 
 /*
-## Problem 2: Object Method Context
+################################################# Problem 2: Object Method Context
 
 Create an object:
 const user = {
@@ -38,12 +38,70 @@ Then store the method in another variable and call it.
 - Call Site Concept
 */
 
-const user = {
-  name: "Anubhav",
-  greed : function(){
-    console.log("hello", this.name)
-  }
-};
+// const user = {
+//   name: "Anubhav",
+//   greed : function(){
+//     console.log("hello", this.name)
+//   }
+// };
 
-let check = user.greed
-check()
+// let check = user.greed
+// check()
+
+
+//################################### Fix using bind()
+// const fn = user.greet.bind(user);
+
+// fn();
+
+// Output:
+
+// Hello Anubhav
+
+/*
+######################################## Problem 3: Arrow Function vs Regular Function
+
+Create an object with:
+name: "Rahul"
+
+Implement:
+
+- One regular method
+- One arrow method
+
+Print `this.name` from both.
+
+### Expected Learning
+
+- Lexical `this`
+- Object Method Context
+*/
+
+// let user = {
+//     name : "aryan",
+//     greet(){
+//         console.log("hello", this.name)
+//     },
+//     greed : ()=>{
+//         console.log("hello", this.name)
+//     }
+    
+// }
+
+// user.greet()
+// user.greed()
+
+
+// let user = {
+//     name : "aryan",
+//     greet(){
+//         let greed = () => {
+//             console.log("hello", this.name)
+//         }
+//         return greed
+//     },
+    
+// }
+
+// let check = user.greet()
+// check()
